89f21f7a90357ef6ca9454db49a7f861b7553d65
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2024-12-03 16:57:43 +0530

    Eg of Recursion

---
+++ DSA/src/com/java/recursionTest/ExponentiationCalculator.java
@@ -0,0 +1,22 @@
+/* Java recursive method to calculate the exponentiation of a number (base) raised to a power (exponent).*/
+package com.java.recursionTest;
+
+public class ExponentiationCalculator {
+	  public static double calculateExponentiation(double base, int exponent) {
+		    // Base case: any number raised to the power of 0 is 1
+		    if (exponent == 0) {
+		      return 1;
+		    }
+
+		    // Recursive case: multiply the base with the exponentiation of (base, exponent-1)
+		    return base * calculateExponentiation(base, exponent - 1);
+		  }
+
+		  public static void main(String[] args) {
+		    double base = 3.5;
+		    int exponent = 4;
+		    double result = calculateExponentiation(base, exponent);
+		    System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+		  }
+
+}
