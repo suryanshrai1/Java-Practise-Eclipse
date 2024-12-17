1766509cb47ec41d3ab0ae86535b5a8ac50ddddc
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2024-12-17 23:57:59 +0530

    String with P Q

---
+++ Practice_codes/src/com/java/StringTest/StringWithPQ.java
@@ -0,0 +1,20 @@
+package com.java.StringTest;
+
+public class StringWithPQ {
+	 
+	   public static void main(String[] args) {
+		   
+		    System.out.println(validate("p"));
+			System.out.println(validate("pq"));
+			System.out.println(validate("pqq"));		
+			System.out.println(validate("pqr"));
+			System.out.println(validate("prr"));
+	        }
+
+	   public static String validate(String text) {
+		   if (text.matches("pq*?"))
+	                return "Found a match!";
+	        else
+	                return "Not matched!";
+	   }
+}
