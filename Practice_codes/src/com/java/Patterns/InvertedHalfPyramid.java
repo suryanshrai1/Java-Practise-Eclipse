ac205ee868b91e90fba19c850750e93ede3cfb10
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2024-12-28 19:47:52 +0530

    Aggregation

--- Practice_codes/src/com/java/Patterns/InvertedHalfPyramid.java
+++ Practice_codes/src/com/java/Patterns/InvertedHalfPyramid.java
@@ -3,7 +3,7 @@ package com.java.Patterns;
 public class InvertedHalfPyramid {
 	public static void main(String[] args) {
 		int n =4;
-		for(int i=1; i<=n; i++) {
+		for(int i=n; i>=1; i--) {
 			for(int j=1;  j<=n-1;  j++) {
 				System.out.print(" ");
 			}
--- Practice_codes/src/com/java/Patterns/InvertedHalfPyramidRotated.java
+++ Practice_codes/src/com/java/Patterns/InvertedHalfPyramidRotated.java
@@ -5,7 +5,7 @@ public class InvertedHalfPyramidRotated {
 	public static void main(String[] args) {
 		int n =4;
 		for(int i=1; i<=n; i++) {
-			for(int j=1;  j<=n-1;  j++) {
+			for(int j=1;  j<=n-i;  j++) {
 				System.out.print(" ");
 			}
 			for(int j=1;  j<=i;   j++) {
@@ -14,6 +14,4 @@ public class InvertedHalfPyramidRotated {
 			System.out.println();
 		}
 	}
-
-
 }
