2d68d860788b5f395cb8eedb3cffb1d49a2d80ea
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-01-27 23:27:15 +0530

    Majority Element

---
+++ DSA/src/com/java/array/MajorityElem.java
@@ -0,0 +1,22 @@
+package com.java.array;
+import java.util.Arrays;
+
+public class MajorityElem {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    public static void main(String[] args) {
+    	MajorityElem solution = new MajorityElem();
+        
+        // Example input
+        int[] nums = {3, 2, 3};
+        
+        // Finding the majority element
+        int majorityElement = solution.majorityElement(nums);
+        
+        // Printing the result
+        System.out.println("The majority element is: " + majorityElement);
+    }
+}
