3c3feabeb990e5ef1f68225e8a3301e316302188
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-01-29 14:06:49 +0530

    remove Element

--- DSA/src/RemoveELement.java
+++ DSA/src/RemoveELement.java
@@ -1,16 +1,29 @@
+public class RemoveElement {
 
-public enum RemoveELement {
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
 
-
-int removeElement(int* nums, int numsSize, int val) {
-    int i = 0;
-    
-    for (int j = 0; j < numsSize; j++) {
-        if (nums[j] != val) {
-            nums[i] = nums[j];
-            i++;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
         }
+        return i; // New length of the array after removal
     }
-    return i;
+
+    public static void main(String[] args) {
+        // Example usage
+        int[] nums = {3, 2, 2, 3, 4, 5, 3};
+        int val = 3;
+
+        int newSize = removeElement(nums, val);
+
+        System.out.println("New size: " + newSize);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < newSize; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
 }
-{
