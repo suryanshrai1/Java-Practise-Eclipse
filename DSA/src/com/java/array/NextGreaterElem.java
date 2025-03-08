cde17be936bead10aab7802d6dd2fb6dce5e9a51
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-03-08 23:24:03 +0530

    sevlet eg..

---
+++ DSA/src/com/java/array/NextGreaterElem.java
@@ -0,0 +1,8 @@
+/* given two arr nums[i] and nums[2] find next greater element for each element of nums1[] in nums[2], if not present return -1
+ */
+
+package com.java.array;
+
+public class NextGreaterElem {
+
+}
