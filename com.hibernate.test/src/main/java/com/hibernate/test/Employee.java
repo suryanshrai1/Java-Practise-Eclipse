9db2bd6b1f6ff732648741a5eaa77b54e3ec7f20
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-03-18 01:04:44 +0530

    Fixed the bug in hibernate.hbm.xml for Employee

--- com.hibernate.test/src/main/java/com/hibernate/test/Employee.java
+++ com.hibernate.test/src/main/java/com/hibernate/test/Employee.java
@@ -1,21 +1,23 @@
 package com.hibernate.test;
 
 public class Employee {
-	
-	int id;
-	String name;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+    private int id;
+    private String name;
 
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- com.hibernate.test/src/main/java/com/hibernate/test/Tester.java
+++ com.hibernate.test/src/main/java/com/hibernate/test/Tester.java
@@ -7,28 +7,22 @@ import org.hibernate.cfg.Configuration;
 
 public class Tester {
 
-	public static void main(String[] args) {
-
-		Configuration configuration = new Configuration();
-		configuration.configure("hibernate.cfg.xml");
-		System.out.println("1");
-
-		SessionFactory sessionFactory = configuration.buildSessionFactory();
-		Session session = sessionFactory.openSession();
-
-		Transaction transaction = session.beginTransaction();
-		Employee employee = new Employee();
-
-		employee.setId(13);
-//	
-		employee.setName("gggg");
-//	
-		session.save(employee);
-
-		// transaction.commit();
-		session.close();
-		System.out.println("2");
-
+	 public static void main(String[] args) {
+		 Configuration configuration=new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			System.out.println("Configuration done !!");
+			//SessionFactory sessionFactory=configuration.buildSessionFactory();
+			SessionFactory sessionFactory=configuration.buildSessionFactory();
+			Session session=sessionFactory.openSession();
+			Transaction transaction=session.beginTransaction();
+			Employee employee=new Employee();
+			employee.setId(3);
+			employee.setName("Ashwani");
+			//session.save(employee);
+			session.save(employee);
+			transaction.commit();
+			session.close();
+			System.out.println("Employee added !");
 	}
 
 }
