64600959955ce5dc75326c38aeb887ffdd1af744
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-03-17 23:40:30 +0530

    Hibernate Test

--- com.hibernate.test/src/main/java/com/hibernate/test/Employee.java
+++ com.hibernate.test/src/main/java/com/hibernate/test/Employee.java
@@ -18,5 +18,4 @@ public class Employee {
 		this.name = name;
 	}
 
-
 }
--- com.hibernate.test/src/main/java/com/hibernate/test/display.java
+++ com.hibernate.test/src/main/java/com/hibernate/test/display.java
@@ -1,5 +1,24 @@
 package com.hibernate.test;
 
-public class display {
+import java.util.Scanner;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class display {
+	public static void main(String[] args) {
+		
+		Configuration configuration=new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		SessionFactory factory=configuration.buildSessionFactory();
+		Session session=factory.openSession();
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enetr id");
+
+		Employee employee=session.get(Employee.class,scanner.nextInt());
+		System.out.println(employee.getName());
+		factory.close();
+	}
+	
 }
