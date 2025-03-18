97a358694dd97cfc3dbf5d05c470fa1618c5483c
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-03-18 14:57:41 +0530

    JPA Eg. JPATest

---
+++ JPATest/src/main/java/Tester.java
@@ -0,0 +1,23 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("emp1");
+		EntityManager entityManager=factory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Employee employee=new Employee();
+		//employee.setId(50);
+		employee.setName("Suryansh");
+		entityManager.persist(employee);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		factory.close();
+		
+		
+	}
+
+}
