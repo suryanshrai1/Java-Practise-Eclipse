a2e7df95217cb8a8b76b832014ede82c6d7fa607
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-03-19 14:13:38 +0530

    CRUD in database through hibernate

--- com.hibernate.test/src/main/java/com/hibernate/test/Delete.java
+++ com.hibernate.test/src/main/java/com/hibernate/test/Delete.java
@@ -6,18 +6,24 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class display {
+public class Delete {
+
 	public static void main(String[] args) {
-		
 		Configuration configuration=new Configuration();
 		configuration.configure("hibernate.cfg.xml");
 		SessionFactory factory=configuration.buildSessionFactory();
 		Session session=factory.openSession();
+		session.getTransaction().begin();
 		Scanner scanner=new Scanner(System.in);
-		System.out.println("enetr id");
+		System.out.println("enetr id to be delete");
+		//String newName=scanner.next();
+		//System.out.println("enter id to update");
+		Employee employee=session.get(Employee.class, scanner.nextInt());
+		session.delete(employee);
+		//employee.setName(newName);
+		//session.save(employee);
+		session.getTransaction().commit();
 
-		Employee employee=session.get(Employee.class,scanner.nextInt());
-		System.out.println(employee.getName());
-		factory.close();
-	}	
+	}
+
 }
--- com.hibernate.test/src/main/java/com/hibernate/test/Update.java
+++ com.hibernate.test/src/main/java/com/hibernate/test/Update.java
@@ -6,18 +6,25 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class display {
+public class Update {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
 		Configuration configuration=new Configuration();
 		configuration.configure("hibernate.cfg.xml");
 		SessionFactory factory=configuration.buildSessionFactory();
 		Session session=factory.openSession();
+		session.getTransaction().begin();
 		Scanner scanner=new Scanner(System.in);
-		System.out.println("enetr id");
+		System.out.println("enter  name");
+		String newName=scanner.next();
+		System.out.println("enter id to update");
+		Employee employee=session.get(Employee.class, scanner.nextInt());
+		employee.setName(newName);
+		session.save(employee);
+		session.getTransaction().commit();
 
-		Employee employee=session.get(Employee.class,scanner.nextInt());
-		System.out.println(employee.getName());
-		factory.close();
-	}	
+	}
+
 }
