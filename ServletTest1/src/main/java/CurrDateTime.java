4979456106afa64c1b63eed0a78d80150e77d4c8
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-03-27 10:55:10 +0530

    CurrDateTime through Servlet

---
+++ ServletTest1/src/main/java/CurrDateTime.java
@@ -0,0 +1,50 @@
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * Servlet implementation class CurrDateTime
+ */
+@WebServlet("/CurrDateTime")
+public class CurrDateTime extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CurrDateTime() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        // Get current date and time
+        Date currentDate = new Date(); 
+        
+        // Print required output
+        out.println("<h2>Today's Date & TIme: " + currentDate + "</h2>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
+
